dacf5db348793a75359a398614d4f0eddb31bbe6
sj0724 <sj07245@naver.com>
2025-12-08 00:24:39 +0900

    inint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,70 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import path from 'path';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
+import tailwindPlugin from 'eslint-plugin-tailwindcss';
+import unusedImports from 'eslint-plugin-unused-imports';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  {
+    files: ['**/*.{js,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      globals: {
+        React: true,
+        JSX: true,
+      },
+    },
 
-export default eslintConfig;
+    plugins: {
+      react: reactPlugin,
+      tailwindcss: tailwindPlugin,
+      'unused-imports': unusedImports,
+      '@typescript-eslint': tsPlugin,
+      import: importPlugin,
+    },
+
+    settings: {
+      tailwindcss: {
+        config: path.join(process.cwd(), 'tailwind.config.js'),
+      },
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'import/no-unresolved': 'off',
+    },
+  },
+
+  // ignore
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+]);
