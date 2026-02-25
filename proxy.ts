aa550b13d92a33f1a22caef0838f6b0fec4cfe80
sj0724 <sj07245@naver.com>
2026-02-26 00:03:50 +0900

    Feat: 어드민 페이지 페스워드 등록

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,28 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url));
+  const basicAuth = request.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pass] = atob(authValue).split(':');
+
+    if (
+      user === process.env.BASIC_AUTH_ID &&
+      pass === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Authentication required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
 }
 
 export const config = {
-  matcher: '/about/:path*',
+  matcher: ['/', '/admin/:path*'],
 };
