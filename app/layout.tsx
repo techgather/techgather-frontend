4f0ffd321458c4d7631be04a727b2d647d912d7b
sj0724 <sj07245@naver.com>
2026-03-30 23:54:14 +0900

    feat: 파비콘, 오픈 그래프 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import localFont from 'next/font/local';
 import Footer from './_components/Footer';
-import Header from './_components/Header';
 import Providers from './_components/Provider';
 
 const Pretendard = localFont({
@@ -21,8 +20,37 @@ const Pretendard = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'DevPick',
-  description: 'developer blog archive',
+  metadataBase: new URL('https://dev-pick.com'),
+  title: {
+    default: 'DevPick',
+    template: '%s | DevPick',
+  },
+  description:
+    '테크 블로그들의 최신 아티클을 현직 개발자가 직접 엄선하여 게재합니다',
+  openGraph: {
+    title: 'DevPick',
+    description:
+      '테크 블로그들의 최신 아티클을 현직 개발자가 직접 엄선하여 게재합니다',
+    url: 'https://dev-pick.com',
+    siteName: 'DevPick',
+    locale: 'ko_KR',
+    type: 'website',
+    images: [
+      {
+        url: '/images/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'DevPick - 테크 블로그 아티클 큐레이션',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'DevPick',
+    description:
+      '테크 블로그들의 최신 아티클을 현직 개발자가 직접 엄선하여 게재합니다',
+    images: ['/images/opengraph-image.png'],
+  },
 };
 
 export default function RootLayout({
