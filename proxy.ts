48993af6589e33087130b43814d62b8b4a3ada76
sj0724 <sj07245@naver.com>
2026-04-11 00:22:50 +0900

    fix: 어드민 인증 제외

--- proxy.ts
+++ proxy.ts
@@ -1,28 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function proxy(request: NextRequest) {
-  const basicAuth = request.headers.get('authorization');
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1];
-    const [user, pass] = atob(authValue).split(':');
-
-    if (
-      user === process.env.BASIC_AUTH_ID &&
-      pass === process.env.BASIC_AUTH_PASSWORD
-    ) {
-      return NextResponse.next();
-    }
-  }
-
-  return new NextResponse('Authentication required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  });
+  return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/admin/:path*'],
-};
