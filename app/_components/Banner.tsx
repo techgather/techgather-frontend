d180a11b01cd14be12931c451f0175ea62dd4d05
hy0909 <pongg005@naver.com>
2026-07-12 06:19:06 +0900

    Update ShaderGradient sphere preset

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -31,35 +31,35 @@ const Banner = () => {
           >
             <ShaderGradient
               animate="on"
-              brightness={1}
-              cAzimuthAngle={180}
-              cDistance={2.8}
-              cPolarAngle={80}
-              cameraZoom={9.1}
-              color1="#3ca283"
-              color2="#8d7dca"
-              color3="#212121"
+              brightness={0.8}
+              cAzimuthAngle={270}
+              cDistance={0.5}
+              cPolarAngle={180}
+              cameraZoom={15.09}
+              color1="#73bfc4"
+              color2="#2fa876"
+              color3="#8da0ce"
               envPreset="city"
               grain="on"
-              lightType="3d"
-              positionX={0}
+              lightType="env"
+              positionX={-0.1}
               positionY={0}
               positionZ={0}
               range="disabled"
               rangeEnd={40}
               rangeStart={0}
-              reflection={0.1}
-              rotationX={50}
-              rotationY={0}
-              rotationZ={-60}
+              reflection={0.4}
+              rotationX={0}
+              rotationY={130}
+              rotationZ={70}
               shader="defaults"
-              type="waterPlane"
-              uAmplitude={0}
-              uDensity={1.5}
-              uFrequency={0}
+              type="sphere"
+              uAmplitude={3.2}
+              uDensity={0.8}
+              uFrequency={5.5}
               uSpeed={0.3}
-              uStrength={1.5}
-              uTime={8}
+              uStrength={0.3}
+              uTime={0}
               wireframe={false}
             />
           </ShaderGradientCanvas>
